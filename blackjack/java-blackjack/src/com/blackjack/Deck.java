9ad038b25c666387e13ba0bbd39df35401af6cd4
Wonsang <temporaryaion1@hotmail.com>
2022-07-03 11:13:50 +0900

    implement Deck methods

--- blackjack/java-blackjack/src/com/blackjack/Deck.java
+++ blackjack/java-blackjack/src/com/blackjack/Deck.java
@@ -1,18 +1,38 @@
 package com.blackjack;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Deck {
     public List<Card> cards;
 
-    public Deck() {}
+    public Deck() {
+        cards = generateDeck();
+        shuffle();
+    }
+
+    public List<Card> generateDeck() {
+        List<Card> theCards = new ArrayList<>();
+        Suit[] suits = new Suit[]{Suit.CLUBS, Suit.SPADES, Suit.DIAMONDS, Suit.HEARTS};
+        String[] values = new String[]{"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+
+        for (Suit suit : suits) {
+            for (String value : values) {
+                theCards.add(new Card(suit, value, false));
+            }
+        }
+
+        return theCards;
+    }
 
     public void shuffle() {
-        // TODO: shuffle cards
-    };
+        cards.sort((c1, c2) -> Double.compare(Math.random(), 0.5));
+    }
+
 
     public Card dealCard(boolean isFaceUp) {
-        // TODO: implement deal card
-        return null;
+        Card card = cards.remove(cards.size() - 1);
+        card.isFaceUp = isFaceUp;
+        return card;
     }
 }
